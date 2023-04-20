03b97ad42d2aef02127dd68dd6207280c0da0e41
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-04-20 19:30:47 -0300

    0.6
    estable
    make script and style media queries

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,12 @@ const year = document.querySelector("#year")
 const label = document.querySelectorAll("#form label");
 const btn = document.querySelector("#form button");
 
+const yearsOut = document.querySelector("#ageYear")
+
+const monthsOut = document.querySelector("#ageMonth")
+
+const daysOut = document.querySelector("#ageDay")
+
 let mensageError = false;
 
 const err = {
@@ -12,6 +18,8 @@ const err = {
     invalid: "Must be a valid",
     dataInvalid: "Must be a valid date"
 }
+
+let date = undefined;
 
 let currentDate = new Date();
 
@@ -23,9 +31,16 @@ btn.addEventListener ("click", (e) => {
     if (mensageError === true) {
         deleteMensageError()
     }
-    validateDay(day.value)
-    validateMonth(month.value)
-    validateYear(year.value)
+    validateDay((parseInt(day.value)))
+    validateMonth((parseInt(month.value)))
+    validateYear((parseInt(year.value)))
+    validateDate((parseInt(day.value)), (parseInt(month.value)), (parseInt(year.value)))
+
+    if (mensageError === false) {
+
+        compareDate(date)
+    }
+
 })
 
 function validateDay(day) {
@@ -42,7 +57,7 @@ function validateMonth(month) {
         displayError(err.empty, "month")
     }
     else if (month < 1 || month > 12) {
-        displayError(err.invalid, "month"")
+        displayError(err.invalid, "month")
     }
 }
 
@@ -57,7 +72,16 @@ function validateYear(year) {
 
 function displayError(error, type) {
     const erro = document.createElement("span");
-    erro.textContent = `${error}`;
+
+    if (error === err.invalid
+        
+        ) {
+        erro.textContent = `${error} ${type}`;
+    }
+    else {
+        erro.textContent = `${error}`;
+    }
+    
     erro.classList.add("erro-span");
 
     if (type === "day") {
@@ -89,4 +113,58 @@ function deleteMensageError() {
 function removeItem(item) {
     let x = item;
     x.remove()
+}
+
+function validateDate(day, month, year) {
+    let dayMax = dayPerMonth(month, year);
+    if (day > dayMax) {
+        displayError(err.dataInvalid)
+        return
+    }
+    createDate(day, month, year)
+}
+
+function createDate(day, month, year) {
+    date = new Date(`${year}-${month}-${day}`)
+}
+
+function compareDate(date) {
+
+    let ageYear = currentDate.getYear() - date.getYear();
+    yearsOut.textContent = `${ageYear}`;
+
+    if (date.getMonth() <= currentDate.getMonth()) {
+        let ageMonth = currentDate.getMonth() - date.getMonth();
+        monthsOut.textContent = `${ageMonth}`;
+    }
+    else {
+        let ageMonth = currentDate.getMonth() - date.getMonth() + 12;
+        monthsOut.textContent = `${ageMonth}`;
+    }
+
+    if (date.getDate() <= currentDate.getDate()) {
+        let ageDay = currentDate.getDate() - date.getDate();
+        daysOut.textContent = `${ageDay}`;
+    }
+    else {
+        let dayMax = dayPerMonth(date.getMonth(), date.getYear())
+        let ageDay = currentDate.getDate() - date.getDate() + dayMax;
+        daysOut.textContent = `${ageDay}`;
+    }
+}
+
+let dayPerMonth = (month, year) => {
+    switch(month) {
+        case 1: case 3: case 5: case 7: 
+        case 8: case 10: case 12:
+            return 31;
+        case 4: case 6:
+        case 9: case 11:
+            return 30;
+        case 2:
+        if ( (year % 400 === 0) || (year % 4 === 0 && year % 100 != 0) )
+            return 29;
+        else
+            return 28;
+       }
 }
